21c9480c2c664acd155a329815a027d14db91948
Andrii Mavdryk <andrii.mavdryk@globallogic.com>
2021-03-05 18:05:38 +0200

    chart.js - initial commit

--- src/common/Loader/Loader.js
+++ src/common/Loader/Loader.js
@@ -4,27 +4,31 @@ import PropTypes from 'prop-types'
 
 import './loader.scss'
 
-const Loader = ({ loaderClassName, wrapperClassName }) => {
-  const loaderClassNames = classnames('loader', loaderClassName)
-  const wrapperClassNames = classnames('wrapper', wrapperClassName)
+const Loader = ({ section, small, secondary }) => {
+  const wrapperClassNames = classnames(
+    'loader-wrapper',
+    section && 'section-loader',
+    small && 'small-loader',
+    secondary && 'secondary-loader'
+  )
 
   return (
     <div className={wrapperClassNames}>
-      <svg className={loaderClassNames} viewBox="0 0 50 50">
-        <circle className="path" cx="25" cy="25" r="20" fill="none" />
-      </svg>
+      <div className="loader" />
     </div>
   )
 }
 
 Loader.defaultProps = {
-  loaderClassName: '',
-  wrapperClassName: ''
+  section: false,
+  small: false,
+  secondary: false
 }
 
 Loader.propTypes = {
-  loaderClassName: PropTypes.string,
-  wrapperClassName: PropTypes.string
+  section: PropTypes.bool,
+  small: PropTypes.bool,
+  secondary: PropTypes.bool
 }
 
-export default Loader
+export default React.memo(Loader)
